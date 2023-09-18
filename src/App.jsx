d1aa84f6a1ef3cb10818693d6d9b06978162f810
NaraeJang <j.naaaarae@gmail.com>
2023-09-18 17:18:11 -0400

    toggle the button

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,26 @@ import Questions from "./Questions";
 
 const App = () => {
   const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleQuestion = (id) => {
+    // data.filter((question) => question.id !== id);
+
+    // if (id === activeId) {
+    //   return setActiveId(null);
+    // }
+    // return setActiveId(id);
+
+    const newActiveId = id === activeId ? null : id;
+    setActiveId(newActiveId);
+  };
   return (
     <main>
-      <Questions questions={questions} />
+      <Questions
+        questions={questions}
+        activeId={activeId}
+        toggleQuestion={toggleQuestion}
+      />
     </main>
   );
 };
--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
-const SingleQuestion = ({ title, info }) => {
-  const [showInfo, setShowInfo] = useState(false);
+const SingleQuestion = ({ id, title, info, activeId, toggleQuestion }) => {
+  const isActive = id === activeId;
 
   return (
     <article className="question">
@@ -11,12 +11,12 @@ const SingleQuestion = ({ title, info }) => {
           type="button"
           className="question-btn"
           onClick={() => {
-            setShowInfo(!showInfo);
+            toggleQuestion(id);
           }}>
-          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {showInfo && <p>{info}</p>}
+      {isActive && <p>{info}</p>}
     </article>
   );
 };
